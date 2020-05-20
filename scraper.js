92f8477ca19155e932ded6f5adb0cf07280220b9
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-20 12:21:49 +0100

    add scraper

---
+++ scraper.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+const url =
+  "https://www.amazon.co.uk/Best-Sellers-Kindle-Store-eBooks/zgbs/digital-text/341689031/ref=zg_bs";
+
+(async function scrape() {
+  try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+    );
+    await page.goto(url);
+    await page.waitForSelector(".zg-item-immersion");
+    const items = await page.$$(".zg-item-immersion");
+
+    const fullItems = await Promise.all(
+      items.map(async (item) => ({
+        title: await item.$eval(".p13n-sc-truncated", (node) => node.innerText),
+        image: await item.$eval(
+          ".zg-text-center-align img",
+          (node) => node.src
+        ),
+        author: await item.$eval(
+          ".aok-inline-block.zg-item > .a-row.a-size-small",
+          (node) => node.innerText
+        ),
+        price: await item.$eval(".p13n-sc-price", (node) => node.innerText),
+        rating: await item.$eval(
+          ".a-icon-row .a-link-normal",
+          (node) => node.title
+        ),
+      }))
+    );
+
+    fs.writeFile("testing.json", JSON.stringify(fullItems, null, 2), (err) => {
+      if (err) throw err;
+      console.log("Items written in file");
+    });
+
+    await browser.close();
+  } catch (e) {
+    console.log("error", e);
+  }
+})();
